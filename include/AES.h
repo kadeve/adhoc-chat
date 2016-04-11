74b62e7510c5047e5916f43b9a14f97f497bd0da
Jan-Willemn <jan-willem.224@hotmail.com>
2016-04-11 14:33:12 +0200

    Added files via upload

---
+++ include/AES.h
@@ -0,0 +1,17 @@
+/*
+ * AES.h
+ *
+ *  Created on: 7 apr. 2016
+ *      Author: Jan-Willem
+ */
+
+#ifndef AES_H_
+#define AES_H_
+
+using namespace std;
+
+
+string encryption(string message, int en_or_de, int person);
+
+
+#endif /* AES_H_ */
